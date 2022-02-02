51a2ea5acbb3dd465ad6d93e5e86cf273503d28e
Richard Germaine <54249860+rlgerma92@users.noreply.github.com>
2022-02-01 17:09:25 -0700

    updated Game component and added stats, plant component

--- src/components/game/index.jsx
+++ src/components/game/index.jsx
@@ -1,19 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Counter from "./Counter";
 import Timer from "./Timer";
-function Game() {
-  const [click, setClick] = useState(0);
-  const [timer, setTimer] = useState(60);
+
+function Plant({ imWatered }) {
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    if (score === 6) {
+      imWatered();
+    }
+  }, [score]);
+
+  return score < 6 ? (
+    <>
+      <Counter count={score} />
+      <button className='nes-btn is-primary' onClick={() => setScore(score + 1)}>
+        Water me!
+      </button>
+    </>
+  ) : (
+    <p>I'm Watered</p>
+  );
+}
+
+function Stats({ score }) {
   return (
     <>
-      <Timer count={timer} />
+      <h3>Plants grown: {score}</h3>
+    </>
+  );
+}
+function Game() {
+  const [time, setTime] = useState(20);
+  const [numberOfClicks, setNumberOfClicks] = useState(0);
+  const [plants, setPlants] = useState([1]);
+
+  const gameIsDone = time === 0 ? true : false;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+
+    if (time <= 0) {
+      clearTimeout(timer);
+    }
+  }, [time]);
+
+  return !gameIsDone ? (
+    <>
+      <Timer time={time} />
       <div className='game-wrap'>
-        <Counter count={click} />
-        <button className='nes-btn is-primary' onClick={() => setClick(click + 1)}>
-          Water me!
-        </button>
+        {plants.map((plant, index) => {
+          return <Plant key={index} imWatered={() => setPlants([...plants, 1])} />;
+        })}
       </div>
     </>
+  ) : (
+    <Stats score={plants.length} />
   );
 }
 
